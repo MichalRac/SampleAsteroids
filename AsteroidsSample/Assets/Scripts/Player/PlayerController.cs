f7097ae4054910efba75aa63a3c03b9120af595e
Michał Rać <rac.michalmail@gmail.com>
2019-06-16 19:28:27 +0200

    Simple player controller and movment

---
+++ AsteroidsSample/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMovment))]
+public class PlayerController : MonoBehaviour
+{
+    private PlayerMovment movement;
+
+    private void Awake()
+    {
+        movement = GetComponent<PlayerMovment>();
+        Debug.Assert(movement, $"Ref to {typeof(PlayerMovment)} script was not found");
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        movement.InputMovement();
+    }
+}
