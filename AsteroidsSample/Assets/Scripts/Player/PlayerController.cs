8ffeacb9e6dab54a7e43463a2360727584c5e32d
Michał Rać <rac.michalmail@gmail.com>
2019-06-16 20:21:14 +0200

    Simple shooting mechanic (TODO: less logic in UPDATEs)

--- AsteroidsSample/Assets/Scripts/Player/PlayerController.cs
+++ AsteroidsSample/Assets/Scripts/Player/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(PlayerMovment))]
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour, IDestroyable
 {
     private PlayerMovment movement;
 
@@ -11,12 +11,16 @@ public class PlayerController : MonoBehaviour
     {
         movement = GetComponent<PlayerMovment>();
         Debug.Assert(movement, $"Ref to {typeof(PlayerMovment)} script was not found");
-
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         movement.InputMovement();
+    }
+
+    public void Destroy()
+    {
+        Destroy(gameObject);
     }
 }
---
+++ AsteroidsSample/Assets/Scripts/WorldObjects/ForwardingObjectsBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class ForwardingObjectsBehaviour : MonoBehaviour
+{
+    [SerializeField] private float _movementSpeed;
+    private Rigidbody _rb;
+
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        _rb.velocity = _rb.transform.forward * _movementSpeed;
+    }
+
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    
+}
