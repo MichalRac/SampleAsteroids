a6dd69d48ef7af7c9e9dca5f24daf380fb70239a
Michał Rać <rac.michalmail@gmail.com>
2019-06-23 22:21:09 +0200

    Setting up layers, and proper obstacle destructions with new objects spawning

--- AsteroidsSample/Assets/Scripts/WorldObjects/AsteroidBehaviour.cs
+++ AsteroidsSample/Assets/Scripts/WorldObjects/AsteroidBehaviour.cs
@@ -5,37 +5,27 @@ using UnityEngine;
 public class AsteroidBehaviour : ForwardingObjectsBehaviour, IDestroyable
 {
     [SerializeField] private GameObject SpawnedObjectOnDestroy;
-    private const float SPEED_MULTIPLIER_ASTEROID = 0.8f;
+    private const float SPEED_MULTIPLIER_ASTEROID = 0.6f;
 
-    // Start is called before the first frame update
     protected override void Start()
     {
-        // Moving towards a random point on screen
+        // Moving towards a random point on screen with random rotation
         Vector3 initialTargetPoint = new Vector3(Random.Range(-LevelBounds.horExtent, LevelBounds.horExtent), 0.0f, Random.Range(-LevelBounds.verExtent, LevelBounds.verExtent));
         Vector3 targetMovmentVector = initialTargetPoint - gameObject.transform.position;
         _rb.velocity = targetMovmentVector.normalized * basePlayerMovementSpeed * SPEED_MULTIPLIER_ASTEROID;
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        _rb.angularVelocity = Random.rotation.eulerAngles.normalized;
     }
 
     public void Destroy()
     {
         if(SpawnedObjectOnDestroy != null)
         {
-            Instantiate(SpawnedObjectOnDestroy, this.transform.position, this.transform.rotation);
-            Instantiate(SpawnedObjectOnDestroy, this.transform.position, this.transform.rotation);
-        }
-        else
-        {
-            Debug.Log($"No object to spawn on {gameObject.name} being destroyed");
+            Instantiate(SpawnedObjectOnDestroy, this.transform.position + new Vector3(5.0f, 0.0f, 5.0f), this.transform.rotation);
+            Instantiate(SpawnedObjectOnDestroy, this.transform.position - new Vector3(5.0f, 0.0f, 5.0f), this.transform.rotation);
         }
         Destroy(gameObject);
     }
+
 
     protected override void OnTriggerEnter(Collider other)
     {
@@ -43,9 +33,6 @@ public class AsteroidBehaviour : ForwardingObjectsBehaviour, IDestroyable
         if (destroyableObject != null)
         {
             destroyableObject.Destroy();
-            Destroy();
         }
-
     }
-
 }
