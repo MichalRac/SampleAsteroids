5d1539267b823016233789d1fb190c59df0ba6a2
Michał Rać <rac.michalmail@gmail.com>
2019-06-27 23:47:49 +0200

    Audio

--- AsteroidsSample/Assets/Scripts/Other/Particle.cs
+++ AsteroidsSample/Assets/Scripts/Other/Particle.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class Particle : MonoBehaviour
 {
     [SerializeField] private float Lifetime = 2.0f;
+    [SerializeField] private AudioClip audioToPlay;
 
     private void Awake()
     {
+        if(audioToPlay != null)
+        {
+            AudioManager.Instance.PlaySound(audioToPlay);
+        }
         Destroy(gameObject, Lifetime);
     }
 }
---
+++ AsteroidsSample/Assets/Scripts/_MainManagementScripts/AudioManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.LogError("Destroying illegal instance of AudioManager singleton");
+            Destroy(gameObject);
+        }
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.clip = clip;
+        audioSource.PlayOneShot(clip);
+    }
+}
